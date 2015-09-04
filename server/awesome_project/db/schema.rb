872eb12abe7b6aa54149eb410ada6c9ccc3a0764
Mark McDonald <pair+mark.mcdonald.ga@gmail.com>
2015-09-04 16:43:31 -0400

    Use mysql :(

--- server/awesome_project/db/schema.rb
+++ server/awesome_project/db/schema.rb
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20150824030701) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
